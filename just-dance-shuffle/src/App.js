4ba6c58a39ef172e962e2bd76592fca4bc058391
Rodrigo Machado <souza.machado.rodrigo@gmail.com>
2021-03-12 08:14:27 -0300

    feat[front-end]: Integrates front-end and back-end

--- just-dance-shuffle/src/App.js
+++ just-dance-shuffle/src/App.js
@@ -20,110 +20,140 @@ const theme = createMuiTheme({
   }
 })
 
-function Topbar() {
-  return (
-    <div className="Topbar">
-      <h1>
-        Just Dance Shuffle
-      </h1>
-    </div>
-  );
+class Topbar extends React.Component {
+  render() {
+    return (
+      <div className="Topbar">
+        <h1>
+          Just Dance Shuffle
+        </h1>
+      </div>
+    );
+  }
 }
 
-function SongBox() {
-  return (
-    <Grid className="component-margin">
-      <Paper>
-        <div className="card">
-          <h2>I Am The Best (내가 제일 잘 나가)</h2>
-          <h3>Just Dance 2020</h3>
-          <p>2NE1</p>
-          <p>Trio</p>
-        </div>
-      </Paper>
-    </Grid>
-  )
-}
-
-function SongPicker() {
-  const [state, setState] = React.useState({
-    unlimited_checkbox: true,
-  });
-
-  const handleChange = (event) => {
-    setState({ ...state, [event.target.name]: event.target.checked });
-  };
+class SongPicker extends React.Component {
+  constructor(props) {
+    super(props);    
+    this.state = {
+      unlimited_checkbox: true,
+      song: {
+        name: null,
+        artist: null,
+        mode: null,
+        game: null 
+      }
+    };
+  }
   
-  const handleSubmit = async e => {
-    e.preventDefault();
+  handleChange = (event) => {
+    this.setState({ ...this.state, [event.target.name]: event.target.checked });
+  };
+
+  handleSubmit = async e => {
     const response = await fetch('/api/get_song', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ unlimited: true }),
+      body: JSON.stringify({ unlimited: this.state.unlimited_checkbox }),
     });
-    const body = await response.text();
-
-    console.log(body)
-    
-    this.setState({ responseToPost: body });
+    const body = await response.json();
+    console.log(typeof(body))       
+    this.setState({ 
+      song: {
+        name: body['name'],
+        artist: body['artist'],
+        mode: body['mode'],
+        game: body['game'] 
+      }
+    })     
+    console.log(this.state.song)        
   };
 
-  return (
-    <div className="SongPicker component-margin">
-      <form onSubmit={handleSubmit}>
-        <div>
-          <FormControlLabel
-            control={<Checkbox 
-                        checked={state.unlimited_checkbox} 
-                        onChange={handleChange} 
-                        color='primary'
-                        name="unlimited_checkbox" />}
-            label="Allow Just Dance Unlimited"
-          />
+  componentDidMount() { this.handleSubmit() }
+
+  render() {  
+    const hasSong = this.state.song.name
+    let songBox;
+
+    if (hasSong != null) {
+      songBox = <div className="card">
+                  <h2>{this.state.song.name}</h2>
+                  <h3>{this.state.song.game}</h3>
+                  <p>{this.state.song.artist}</p>
+                  <p>{this.state.song.mode}</p>
+                </div>
+    } else {
+      songBox = <div className="card">
+                  <h2>Click the 'Shuffle' button to pick a random song</h2>
+                </div>
+    }
+
+    return (
+      <div className='song-wall'>
+        <Grid className="component-margin">
+          <Paper>
+            <div className="card">
+              <h2>{this.state.song.name}</h2>
+              <h3>{this.state.song.game}</h3>
+              <p>{this.state.song.artist}</p>
+              <p>{this.state.song.mode}</p>
+            </div>
+          </Paper>
+        </Grid>
+        <div className="SongPicker component-margin">
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <FormControlLabel
+                control={<Checkbox 
+                            checked={this.state.unlimited_checkbox} 
+                            onChange={this.handleChange} 
+                            color='primary'
+                            name="unlimited_checkbox" />}
+                label="Allow Just Dance Unlimited"
+              />
+            </div>
+            <Button 
+              variant='outlined' 
+              color='default' 
+              background='mediumpurple'
+              endIcon={<ShuffleIcon/>}
+              onClick={this.handleSubmit}
+              >
+                Shuffle
+            </Button>
+          </form>
         </div>
-        <Button 
-          variant='outlined' 
-          color='default' 
-          background='mediumpurple'
-          endIcon={<ShuffleIcon/>}>
-            Shuffle
-        </Button>
-      </form>
-    </div>
-  );
-}
-
-function MainContainer() {
-  return (
-    <div className='song-wall'>
-      <SongBox></SongBox>
-      <SongPicker></SongPicker>
-    </div>
-  )
-}
-
-function Footer() {
-  return (
-    <footer className="Footer">
-      <div className="contact-info">
-        <p>Author: Rodrigo Machado</p>
       </div>
-    </footer> 
-  )
+    );
+  }
 }
 
-function App() {
-  return(
-    <ThemeProvider theme={theme}>
-      <div>
-        <Topbar />
-        <MainContainer />
-        <Footer />
-      </div>
-    </ThemeProvider>
-  )
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="Footer">
+        <div className="contact-info">
+          <p>Author: Rodrigo Machado</p>
+        </div>
+      </footer> 
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return(
+      <ThemeProvider theme={theme}>
+        <div>
+          <Topbar />
+          <SongPicker />
+          <Footer />
+        </div>
+      </ThemeProvider>
+    )
+  }
 }
 
 export default App;
